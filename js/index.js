20c4bb22c37d3f3027fa0e5fcf279af8bf91c172
AndrijMiso <misoandrij@gmail.com>
2020-03-19 02:34:54 +0300

    code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 var link = document.querySelector('a');
+
+link.textContent = 'This is sait my Mew Car!';
+link.href = 'https://www.facebook.com/groups/1068991656791818/';
+
+
+var sect = document.querySelector('section');
+var para = document.createElement('p');
+sect.appendChild(para);
+para.textContent = 'We hope you enjoyed the ride.';
+
+// for (let i = 1; i <= 1000; i++) {
+//     const p = document.createElement('p')
+//     p.textContent = i + ' sam text...';
+//     sect.appendChild(p);
+// }
+
+const clock = document.querySelector('.clock');
+const showClock = () => clock.innerText = (new Date()).toLocaleTimeString(); 
+setInterval(showClock, 10000);
