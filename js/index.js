f0560495d201068c6a254a7b9490a00fe14e601d
AndrijMiso <misoandrij@gmail.com>
2020-03-19 23:22:13 +0300

    code

--- js/index.js
+++ js/index.js
@@ -24,4 +24,21 @@ $('.click-me').click( () => $('img').toggle(3000) );
 const display = document.querySelector('.display');
 
 document.querySelectorAll('.digits button')
-    .forEach( button => button.addEventListener('click', digitPressed));
+.forEach( button => button.addEventListener('click', digitPressed));
+
+function digitPressed(ev) {
+display.value += ev.target.innerText;
+}
+
+document.querySelectorAll('.opers button')
+    .forEach( button => button.addEventListener('click', operPressed));
+
+function operPressed(ev) {
+    display.value += ev.target.innerText;
+}
+
+document.querySelector('.eq').addEventListener('click', calculate);
+
+function calculate() {
+    display.value = eval(display.value);
+}
